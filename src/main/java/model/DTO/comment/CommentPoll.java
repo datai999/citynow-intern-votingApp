4dfffcaae214a6f17b6be15c7523e5bb13572f23
Đức Anh Tài <39086667+datai999@users.noreply.github.com>
2020-08-07 11:50:34 +0700

    mer

--- src/main/java/model/DTO/comment/CommentPoll.java
+++ src/main/java/model/DTO/comment/CommentPoll.java
@@ -1,17 +1,43 @@
 package model.dto.comment;
 
-import model.dto.poll.Poll;
+
 import model.dto.user.UserAccount;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Comment {
+public class CommentPoll {
 
 //    Same database
     int id;
     int pollId;
     int userId;
+
+    int timeCreate;
+    int replyCommentId;
+    String content;
+
+//    Extend database
+    UserAccount commentator;
+
+    @Override
+    public String toString(){
+        return String.format("Comment[%d,%d,%d,%d,%d,%s]",id,pollId,userId,timeCreate,replyCommentId,content);
+    }
+
+
+    public CommentPoll(ResultSet rs) throws SQLException {
+        this.id = rs.getInt("comment.id");
+        this.pollId = rs.getInt("comment.pollId");
+        this.userId = rs.getInt("comment.userId");
+        this.timeCreate = rs.getInt("comment.timeCreate");
+        this.replyCommentId = rs.getInt("replyCommentId");
+        this.content = rs.getString("comment.content");
+    }
+
+    public int getPollId() {
+        return pollId;
+    }
 
     public int getTimeCreate() {
         return timeCreate;
@@ -21,37 +47,15 @@ public class Comment {
         return content;
     }
 
-    int timeCreate;
-    int replyCommentId;
-    String content;
-
-//    Extend database
-    UserAccount commentByUser;
-    Poll commentOnPoll;
-
-    @Override
-    public String toString(){
-        return String.format("Comment[%d,%d,%d,%d,%d,%s]",id,pollId,userId,timeCreate,replyCommentId,content);
+    public UserAccount getCommentator() {
+        return commentator;
     }
 
-    public UserAccount getCommentByUser() {
-        return commentByUser;
+    public void setCommentator(UserAccount commentator) {
+        this.commentator = commentator;
     }
 
-    public void setCommentByUser(UserAccount commentByUser) {
-        this.commentByUser = commentByUser;
-    }
-
-    public Comment(ResultSet rs) throws SQLException {
-        this.id = rs.getInt("comment.id");
-        this.pollId = rs.getInt("comment.pollId");
-        this.userId = rs.getInt("comment.userId");
-        this.timeCreate = rs.getInt("comment.timeCreate");
-        this.replyCommentId = rs.getInt("replyCommentId");
-        this.content = rs.getString("comment.content");
-    }
-
-    public Comment(int pollId, int userId, int replyCommentId, String content){
+    public CommentPoll(int pollId, int userId, int replyCommentId, String content){
         this.pollId = pollId;
         this.userId = userId;
         this.timeCreate =  (int) (System.currentTimeMillis()/1000);
